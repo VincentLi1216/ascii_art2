5af10959801180cde9d0163b147022eb33748654
VincentLi1216 <sunnus.tw@gmail.com>
2023-12-21 23:37:07 +0800

    finish mapping

--- util.py
+++ util.py
@@ -1,11 +1,13 @@
-l1 = [1,2,3,4]
 def mapping(list, down_boun, up_boun):
     _max = max(list)
     _min = min(list)
     normalized = [(x-_min)/(_max-_min) for x in list]
     ans = [down_boun+x*(up_boun-down_boun) for x in normalized]
-    print(normalized)
-    print(ans)
     return ans
 
-mapping(l1, 0, 255)
+
+
+
+if __name__ == "__main__":
+    l1 = [1,2,3,4]
+    print(mapping(l1, 0, 255))
